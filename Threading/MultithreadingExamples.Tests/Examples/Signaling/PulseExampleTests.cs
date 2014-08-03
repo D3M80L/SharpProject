235d4893474cd328e50639a8bcf07c575445fe35
D3M80L <marcin.dembowski@gmail.com>
2014-08-03 19:12:12 +0200

    Added Pulse Examples

---
+++ Threading/MultithreadingExamples.Tests/Examples/Signaling/PulseExampleTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultithreadingExamples.Examples.Signaling;
+using MultithreadingExamples.Tests.Infrastructure;
+using NUnit.Framework;
+
+namespace MultithreadingExamples.Tests.Examples.Signaling
+{
+    [TestFixture]
+    public sealed class PulseExampleTests : ExampleTestBase<PulseExample>
+    {
+        [Test]
+        public void Pulse_OnlyOneWaitingThreadIsPulsed()
+        {
+            // Arrange
+            Example.UsePulseAllInsteadOfPulse = false;
+
+            var countPadLockExitState = new CatchStateObserver(x => x.StartsWith(PulseExample.ExitedPadLock));
+            StateMachine.AddObserver(countPadLockExitState);
+
+            // Act
+            RunExampleInThread();
+            WaitForExitConfirmation(3000);
+
+            // Assert
+            Assert.AreEqual(1, countPadLockExitState.Count);
+        }
+
+        [Test]
+        public void PulseAll_AllWaitingThreadsShouldContinue()
+        {
+            // Arrange
+            Example.UsePulseAllInsteadOfPulse = true;
+
+            var countPadLockExitState = new CatchStateObserver(x => x.StartsWith(PulseExample.ExitedPadLock));
+            StateMachine.AddObserver(countPadLockExitState);
+
+            // Act
+            RunExampleInThread();
+            WaitForExitConfirmation(5000);
+
+            // Assert
+            Assert.AreEqual(4, countPadLockExitState.Count);
+        }
+    }
+}
---
+++ Threading/MultithreadingExamples/Examples/Signaling/PulseExample.cs
@@ -0,0 +1,105 @@
+﻿using System.Threading;
+using MultithreadingExamples.Infrastructure;
+using MultithreadingExamples.Infrastructure.Extensions;
+
+namespace MultithreadingExamples.Examples.Signaling
+{
+    /// <summary>
+    /// Important:
+    /// Calling Pulse or PulseAll before any thread is waiting for pulse causes, that the call is swallowed
+    /// </summary>
+    public sealed class PulseExample : ExampleBase
+    {
+        public const string WaitForPulseMessage = "WaitForPulseMessage";
+        public const string EnteredPadLock = "EnteredPadLock";
+        public const string ExitedPadLock = "ExitedPadLock";
+        public const string BeforePulse = "BeforePulse";
+        public const string AfterPulse = "AfterPulse";
+        public const string BeforePulseAll = "BeforePulseAll";
+        public const string AfterPulseAll = "AfterPulseAll";
+        public const string Pulsing = "Pulsing";
+
+        private readonly object _padLock = new object();
+        private readonly CountdownEvent _countdownEvent = new CountdownEvent(4);
+
+        public bool UsePulseAllInsteadOfPulse { get; set; }
+
+
+        protected override void OnRun()
+        {
+            ThreadPool.QueueUserWorkItem(_ => WaitForPulse("A"));
+            ThreadPool.QueueUserWorkItem(_ => WaitForPulse("B"));
+            ThreadPool.QueueUserWorkItem(_ => WaitForPulse("C"));
+            ThreadPool.QueueUserWorkItem(_ => WaitForPulse("D"));
+
+            EnsureAllThreadsAreWaitingForPulse();
+            
+            CallPulseOrPulseAll();
+
+            Thread.Sleep(2000);
+        }
+
+        private void CallPulseOrPulseAll()
+        {
+            Log.Info(Pulsing);
+
+            if (UsePulseAllInsteadOfPulse)
+            {
+                PulseAll();
+            }
+            else
+            {
+                Pulse();
+            }
+        }
+
+        private void Pulse()
+        {
+            lock (_padLock)
+            {
+                Log.Info(BeforePulse);
+
+                Monitor.Pulse(_padLock);
+
+                Log.Info(AfterPulse);
+            }
+        }
+
+        private void PulseAll()
+        {
+            lock (_padLock)
+            {
+                Log.Info(BeforePulseAll);
+
+                Monitor.PulseAll(_padLock);
+
+                Log.Info(AfterPulseAll);
+            }
+        }
+
+        private void WaitForPulse(string id)
+        {
+            Log.Info(WaitForPulseMessage + id);
+
+            lock (_padLock)
+            {
+                Log.Info(EnteredPadLock + id);
+
+                NotifyWaitingForPulse();
+                Monitor.Wait(_padLock);
+
+                Log.Info(ExitedPadLock + id);
+            }
+        }
+
+        private void EnsureAllThreadsAreWaitingForPulse()
+        {
+            _countdownEvent.Wait();
+        }
+
+        private void NotifyWaitingForPulse()
+        {
+            _countdownEvent.Signal();
+        }
+    }
+}
