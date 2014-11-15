785f816bf7b57f550f8bd283f36a6caf985ef4a7
D3M80L <marcin.dembowski@gmail.com>
2014-11-15 13:57:06 +0100

    Improved Bouncing Ball example - put into module, and fixed bouncing bug

--- HTML5/HTML5/Samples/100 Canvas/SimpleAnimation.js
+++ HTML5/HTML5/Samples/100 Canvas/SimpleAnimation.js
@@ -1,57 +1,93 @@
-var canvasWidth  = 800;
-var canvasHeight = 600;
-
-$('#CanvasGame').attr('width', canvasWidth);
-$('#CanvasGame').attr('height', canvasHeight);
-
-var canvas2dContext = $('#CanvasGame')[0].getContext('2d');
-
-var ball = new Image();
-ball.src = "ball.png";
-
-var ballRadius = ball.width / 2;
-var ballDiameter = ball.width;
-var ballPosX = (canvasWidth / 2) - ballRadius;
-var ballPosY = (canvasHeight / 2) - ballRadius;
-var ballRotation = 100;
-
-var vX = 1;
-var vY = 1;
-var step = 5;
-
-function changePosition() {
-	ballPosX = ballPosX + vX * step;
-	ballPosY = ballPosY + vY * step;
-	ballRotation = ballRotation + 5;
+"use strict";
+var bouncingBallModule = (function() {
+	var canvasWidth,
+		canvasHeight,
+		canvas2dContext,
+		ballDiameter,
+		ballRadius,
+		ballPosX,
+		ballPosY,
+		ball,
+		ballRotation,
+		vX,
+		vY,
+		step,
+		maxDistanceX,
+		maxDistanceY;
 	
-	if (ballPosX <= 0) {
-		ballPosX = 0;
-		vX *= -1;
-	} else if (ballPosX >= canvasWidth - ballDiameter) {
-		ballPosX = canvasWidth - ballDiameter;
-		vX *= -1;
-	}
+	var init = function() {
+		canvasWidth = 800;
+		canvasHeight = 600;
+		
+		$('#CanvasGame').attr('width', canvasWidth);
+		$('#CanvasGame').attr('height', canvasHeight);
+		canvas2dContext = $('#CanvasGame')[0].getContext('2d');
+		
+		ball = new Image();
+		ball.onload = ballLoaded;
+		ball.src = "ball.png";
+	};
 	
-	if (ballPosY <= 0) {
-		ballPosY = 0;
-		vY *= -1;
-	} else if (ballPosY >= canvasHeight - ballDiameter) {
-		ballPosY = canvasHeight - ballDiameter;
-		vY *= -1;
-	}
-}
-
-function draw() {
-	canvas2dContext.clearRect(0, 0, canvasWidth, canvasHeight);
+	var ballLoaded = function() {
+		ballDiameter = this.width;
+		ballRadius = ballDiameter / 2;
+		ballPosX = (canvasWidth / 2) - ballRadius;
+		ballPosY = (canvasHeight / 2) - ballRadius;
+		ballRotation = 100;
+		vX = 1;
+		vY = 1;
+		step = 5;
+		maxDistanceX = canvasWidth - ballRadius;
+		maxDistanceY = canvasHeight - ballRadius;
+		startAnimation();
+	};
 	
-	canvas2dContext.save();
-	canvas2dContext.translate(ballPosX + ballRadius, ballPosY + ballRadius);
-	canvas2dContext.rotate(ballRotation*Math.PI/180);
-	canvas2dContext.drawImage(ball, -ballRadius, -ballRadius);
-	canvas2dContext.restore();
-}
+	var changePosition = function() {
+		ballPosX = ballPosX + vX * step;
+		ballPosY = ballPosY + vY * step;
+		ballRotation = ballRotation + 5;
+		
+		if (ballPosX <= ballRadius) {
+			ballPosX = ballRadius;
+			vX *= -1;
+		} else if (ballPosX >= maxDistanceX) {
+			ballPosX = maxDistanceX;
+			vX *= -1;
+		}
+		
+		if (ballPosY <= ballRadius) {
+			ballPosY = ballRadius;
+			vY *= -1;
+		} else if (ballPosY >= maxDistanceY) {
+			ballPosY = maxDistanceY;
+			vY *= -1;
+		}
+	};
+	
+	var draw = function draw() {
+		canvas2dContext.clearRect(0, 0, canvasWidth, canvasHeight);
+		
+		canvas2dContext.save();
+		canvas2dContext.translate(ballPosX, ballPosY);
+		canvas2dContext.rotate(ballRotation*Math.PI/180);
+		canvas2dContext.drawImage(ball, -ballRadius, -ballRadius);
+		canvas2dContext.restore();
+		console.log(ballRadius);
+	};
+	
+	var startAnimation = function() {
+		setInterval(function() {
+			changePosition();
+			draw();
+		}, 1000 / 25);
+	};
+	
+	return {
+		run: function() {
+			init();
+		}
+	};
 
-setInterval(function() {
-	changePosition();
-	draw();
-}, 1000 / 25);
+})();
+
+bouncingBallModule.run();
